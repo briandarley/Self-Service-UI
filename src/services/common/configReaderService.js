6d88648833b0da85ffa3ef827692cbeabe7027e1
bdarley <bdarley@email.unc.edu>
2019-04-18 08:39:42 -0400

    modified configuration logic so that it's not backed into the build

--- src/services/common/configReaderService.js
+++ src/services/common/configReaderService.js
@@ -14,21 +14,35 @@ import {
 function ConfigReaderService() {
     return {
         async getConfiguration() {
-           const environment = Config.environment;
+           //const environment = Config.environment;
+           let axios = injector.get("axios");
+           
+           
+           var configuration =  await axios.get('configuration.json');
+           const environment = configuration.data.environment;
+
+
+
 
            if (environment !== "local") {
                if (window.location.protocol !== "https:") {
                    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
                }
            }
-
+           let oidcSettings = {};
            switch (environment) {
                case "local":
-                   return LocalOidcConfig;
+                 oidcSettings = await axios.get('oidc.config.local.json');
+                return oidcSettings.data;
+                   //return LocalOidcConfig;
                case "test":
-                   return TestOidcConfig;
+                oidcSettings = await axios.get('oidc.config.test.json');
+                return oidcSettings.data;
+                   
                case "production":
-                   return ProductionOidcConfig;
+                oidcSettings = await axios.get('oidc.config.production.json');
+               return oidcSettings.data;
+                   //return ProductionOidcConfig;
                default:
                    throw "Environment not found or not supported";
            }
