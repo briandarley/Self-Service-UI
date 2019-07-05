f269f51fece6263863f1afbd27d620951dae42aa
bdarley <bdarley@email.unc.edu>
2019-07-05 11:30:22 -0400

    refactored Add Alias UI

--- src/views/win-tools/exchange-tools/aliases/aliases.vue
+++ src/views/win-tools/exchange-tools/aliases/aliases.vue
@@ -8,70 +8,83 @@
         <h3>Aliases</h3>
       </div>
       <div class="card-body">
-        <div class="form-group">
-          <label for>User Id</label>
-          <input
-            type="text"
-            class="form-control"
-            v-select-all
-            v-model="filter"
-            placeholder="onyen"
-            v-on:keyup.13="search()"
-          >
-        </div>
-        <div class="submit text-right">
-          <button class="btn btn-primary mr-1" @click="search()">Search</button>
-          <button class="btn btn-secondary" @click="clear()">Clear</button>
+        <div class="alert alert-info">
+          <div class="info">
+            <i class="fa fa-info-circle"></i>
+          </div>
+          <div class="p-2">
+            <p>This tool may be used to manage user's email aliases as well as manage the user's forwarding address for Office 365.</p>
+            <p>Enter the Onyen for the person you wish to modify</p>
+          </div>
         </div>
 
-        <div class="container" v-if="adUser">
-          <div class="alert alert-info mt-3">
-            <p>
-                Use the radio buttons on the grid to designate the a primary alias.
-                Some aliases may not be selected or removed as they are required for proper account function.</p>
-            <p>Use the 'Add Email Alias' button to add new aliases for the selected account</p>
-            <p>Use the 'Forward Address' button to set or remove Forwarding Smtp Address for the selected account</p>
+        <div class="container">
+          <div class="form-group">
+            <label for>User Id</label>
+            <input
+              type="text"
+              class="form-control"
+              v-select-all
+              v-model="filter"
+              placeholder="onyen"
+              v-on:keyup.13="search()"
+            >
           </div>
           <div class="submit text-right">
-            <button class="btn btn-primary mr-2" @click="showAddEmailAliasDialog()">
-              <i class="fa fa-plus-circle"></i>
-              <span class="ml-2">Add Email Alias</span>
-            </button>
-            <button class="btn btn-primary" @click="showAddFormwardingAddressDialog()">
-              <i class="fa fa-address-card"></i>
-              <span class="ml-2">Forward Address</span>
-            </button>
+            <button class="btn btn-primary mr-1" @click="search()">Search</button>
+            <button class="btn btn-secondary" @click="clear()">Clear</button>
           </div>
-          <!-- Alias List -->
-          <div class="row bg-primary text-white row-header">
-            <div class="col-1">Alias</div>
-            <div class="col-2 text-center">Is Primary</div>
-            <div class="col-3"></div>
-          </div>
-          <div class="result-grid row" v-for="item in emailAddresses" v-bind:key="item.email">
-            <div class="col-1">{{item.email}}</div>
-            <div class="col-2 radio-buttons text-center">
-              <input
-                type="radio"
-                name="primary"
-                :checked="item.primary"
-                :value="item.email"
-                v-model="primary"
-                :disabled="!item.valid"
-                @change="confirmChangePrimaryAlias($event)"
-              >
-            </div>
 
-            <div class="col-3 text-right">
-              <a
-                href="#"
-                @click.prevent="confrimDeleteEntity(item)"
-                title="delete alias"
-                :disabled="!item.valid"
-                :class="{'text-disabled': !item.valid}"
-              >
-                <i class="fa fa-trash-o"></i>
-              </a>
+          <div class="container" v-if="adUser">
+            <div class="alert alert-info mt-3">
+              <p>
+                Use the radio buttons on the grid to designate the a primary alias.
+                Some aliases may not be selected or removed as they are required for proper account function.
+              </p>
+              <p>Use the 'Add Email Alias' button to add new aliases for the selected account</p>
+              <p>Use the 'Forward Address' button to set or remove Forwarding Smtp Address for the selected account</p>
+            </div>
+            <div class="submit text-right">
+              <button class="btn btn-primary mr-2" @click="showAddEmailAliasDialog()">
+                <i class="fa fa-plus-circle"></i>
+                <span class="ml-2">Add Email Alias</span>
+              </button>
+              <button class="btn btn-primary" @click="showAddFormwardingAddressDialog()">
+                <i class="fa fa-address-card"></i>
+                <span class="ml-2">Forward Address</span>
+              </button>
+            </div>
+            <!-- Alias List -->
+            <div class="row bg-primary text-white row-header">
+              <div class="col-1">Alias</div>
+              <div class="col-2 text-center">Is Primary</div>
+              <div class="col-3"></div>
+            </div>
+            <div class="result-grid row" v-for="item in emailAddresses" v-bind:key="item.email">
+              <div class="col-1">{{item.email}}</div>
+              <div class="col-2 radio-buttons text-center">
+                <input
+                  type="radio"
+                  name="primary"
+                  :checked="item.primary"
+                  :value="item.email"
+                  v-model="primary"
+                  :disabled="!item.valid"
+                  @change="confirmChangePrimaryAlias($event)"
+                >
+              </div>
+
+              <div class="col-3 text-right">
+                <a
+                  href="#"
+                  @click.prevent="confrimDeleteEntity(item)"
+                  title="delete alias"
+                  :disabled="!item.valid"
+                  :class="{'text-disabled': !item.valid}"
+                >
+                  <i class="fa fa-trash-o"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -155,8 +168,7 @@
         <div class="container forwarding-address">
           <div class="alert alert-info">
             <p>Use this control to set or remove the account's Forwarding SMTP Address.</p>
-            <p>
-              The value entered must be a valid email address. If you wish to clear the Forwarding SMTP Address, press the clear button and select 'Set Forwarding Address' button.            </p>
+            <p>The value entered must be a valid email address. If you wish to clear the Forwarding SMTP Address, press the clear button and select 'Set Forwarding Address' button.</p>
           </div>
 
           <div class="form-group add-alias-cmd">
