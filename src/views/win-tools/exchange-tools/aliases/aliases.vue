4e9c27e49de5d9f295e8f5a240594e94fc6e1928
bdarley <bdarley@email.unc.edu>
2019-07-05 11:35:02 -0400

    refactored Add Alias UI

--- src/views/win-tools/exchange-tools/aliases/aliases.vue
+++ src/views/win-tools/exchange-tools/aliases/aliases.vue
@@ -13,7 +13,7 @@
             <i class="fa fa-info-circle"></i>
           </div>
           <div class="p-2">
-            <p>This tool may be used to manage user's email aliases as well as manage the user's forwarding address for Office 365.</p>
+            <p>This tool may be used to manage user's email aliases including, adding, removing, setting primary email alias, as well as manage the user's forwarding address for Office 365.</p>
             <p>Enter the Onyen for the person you wish to modify</p>
           </div>
         </div>
