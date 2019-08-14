9b1d5cec4c4d09054eaeebb1e4daeaadc5e279a1
bdarley <bdarley@email.unc.edu>
2019-08-14 11:09:02 -0400

    oops

--- src/main.js
+++ src/main.js
@@ -177,11 +177,11 @@ new Vue({
     //await  userService.get()
     let referer = window.document.referrer;
     //https://sso-test.isis.unc.edu/idp/profile/SAML2/Redirect/SSO?execution=e1s1
-    // if(referer.indexOf("SAML2")){
-    //   let userService = injector.get("UserService");
-    //   await userService.logout();
-    //   return;
-    // }
+     if(referer.indexOf("SAML2") > -1){
+       let userService = injector.get("UserService");
+       await userService.logout();
+       return
+     }
     
     
     let service = injector.get("RouteSourcesService");
