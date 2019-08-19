c14bc244aeaeb9e1c2e70399d0637b6efa96cd81
bdarley <bdarley@email.unc.edu>
2019-08-19 09:26:02 -0400

    added tabs for managing MFA access

--- src/views/administration/help-desk-access/help-desk-access.vue
+++ src/views/administration/help-desk-access/help-desk-access.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="card-body">
       <div class="container">
-        <tabbed-control tabs="Help Desk Users,Help Desk Postmasters,Help Desk Managers" >
+        <tabbed-control tabs="Help Desk Users,Help Desk Postmasters,Help Desk Managers,MFA Reset, MFA Manage" >
           <tabbed-item slot="tab_0">
             <help-desk-role-management role-to-manage="ITS_WSP-Access-HelpdeskUsers"></help-desk-role-management>
           </tabbed-item>
@@ -18,6 +18,12 @@
           </tabbed-item>
           <tabbed-item slot="tab_2">
             <help-desk-role-management role-to-manage="ITS_WSP-Access-HelpdeskManagers"></help-desk-role-management>
+          </tabbed-item>
+          <tabbed-item slot="tab_3">
+            <help-desk-role-management role-to-manage="ITS_WSP-Tools-MFA-Reset"></help-desk-role-management>
+          </tabbed-item>
+          <tabbed-item slot="tab_4">
+            <help-desk-role-management role-to-manage="ITS_WSP-Tools-MFA-Manage"></help-desk-role-management>
           </tabbed-item>
         </tabbed-control>
         
