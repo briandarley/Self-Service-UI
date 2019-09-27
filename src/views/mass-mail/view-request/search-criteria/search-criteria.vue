d888dbb28e1daa2e4ac32711fdee91088f703b08
bdarley <bdarley@email.unc.edu>
2019-09-27 08:45:11 -0400

    interim check in

--- src/views/mass-mail/view-request/search-criteria/search-criteria.vue
+++ src/views/mass-mail/view-request/search-criteria/search-criteria.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="border border-primary">
+  <div class="border border-primary search-criteria">
     <div class="bg bg-primary text-white p-2">Search Criteria</div>
     <div class="m-2">
-      <div class="d-flex justify-content-between">
-        <div class="form-group flex-grow-1 pr-3 mr-3">
-          <label for="campaign-status">Campaign Status</label>
-          <select
-            name="campaign-status"
-            id="campaign-status"
-            class="form-control"
-            v-model="criteria.status"
-          >
-            <option value="PENDING">Pending Approval (All)</option>
-            <option value="PENDING_STUDENTS">Pending Approval for Students</option>
-            <option value="PENDING_EMPLOYEES">Pending Approval for Employees</option>
-            <option value="EXPIRED">Expired</option>
-            <option value="CANCELED">Canceled</option>
-            <option value="DENIED">Denied</option>
-            <option value="ALL">All</option>
-          </select>
+      <div class="row-1">
+        <div class="campaign-criteria">
+          <div class="form-group">
+            <label for="campaign-status">Campaign Status</label>
+            <select
+              name="campaign-status"
+              id="campaign-status"
+              class="form-control"
+              v-model="criteria.status"
+            >
+              <option value="PENDING">Pending Approval (All)</option>
+              <option value="PENDING_STUDENTS">Pending Approval for Students</option>
+              <option value="PENDING_EMPLOYEES">Pending Approval for Employees</option>
+              <option value="EXPIRED">Expired</option>
+              <option value="CANCELED">Canceled</option>
+              <option value="DENIED">Denied</option>
+              <option value="ALL">All</option>
+            </select>
+          </div>
         </div>
-        <div class="d-flex flex-grow-0">
-          <div class="form-group mr-3">
+
+        <div class="expirary-criteria">
+          <div class="form-group">
             <div>
               <label for="dtExpDtFrom">Expire Date From</label>
               <date-picker
@@ -31,7 +34,7 @@
               ></date-picker>
             </div>
           </div>
-          <div class="form-group flex-fill">
+          <div class="form-group">
             <label for="dtExpDtThru">Expire Date Thru</label>
 
             <date-picker
@@ -42,47 +45,54 @@
           </div>
         </div>
       </div>
-      <div class="d-flex justify-content-between flex-grow-1">
-        <div class="form-group flex-fill mr-3">
-          <label for="selectPopulation">Population</label>
-          <select
-            name="selectPopulation"
-            id="selectPopulation"
-            class="form-control"
-            v-model="criteria.population"
-          >
-            <option value="ALL">All</option>
-            <option value="EMPLOYEES">Employees</option>
-            <option value="STUDENTS">Students</option>
-          </select>
+      <div class="row-2">
+        <div class="population-criteria">
+          <div class="form-group">
+            <label for="selectPopulation">Population</label>
+            <select
+              name="selectPopulation"
+              id="selectPopulation"
+              class="form-control"
+              v-model="criteria.population"
+            >
+              <option value="ALL">All</option>
+              <option value="EMPLOYEES">Employees</option>
+              <option value="STUDENTS">Students</option>
+            </select>
+          </div>
         </div>
-
-        <div class="form-group flex-fill ml-3">
-          <label for="selectPriority">Priority</label>
-          <select
-            name="selectPriority"
-            id="selectPriority"
-            class="form-control"
-            v-model="criteria.priority"
-          >
-            <option value="ALL">All</option>
-            <option value="FORMAL NOTICE">Formal</option>
-            <option value="INFORMATIONAL">Informal</option>
-          </select>
+        <div class="priority-criteria">
+          <div class="form-group">
+            <label for="selectPriority">Priority</label>
+            <select
+              name="selectPriority"
+              id="selectPriority"
+              class="form-control"
+              v-model="criteria.priority"
+            >
+              <option value="ALL">All</option>
+              <option value="FORMAL NOTICE">Formal</option>
+              <option value="INFORMATIONAL">Informal</option>
+            </select>
+          </div>
         </div>
       </div>
-      <div class="form-group">
-        <label for="txt-filter">Filter</label>
-        <input
-          type="text"
-          class="form-control"
-          id="txt-filter"
-          placeholder="Onyen, Subject, Campaign Id"
-          v-select-all
-          v-on:keyup.13="search()"
-          v-model="criteria.filterText"
-          autocomplete="off"
-        />
+      <div class="row-3">
+        <div class="filter-criteria">
+          <div class="form-group">
+            <label for="txt-filter">Filter</label>
+            <input
+              type="text"
+              class="form-control"
+              id="txt-filter"
+              placeholder="Onyen, Subject, Campaign Id"
+              v-select-all
+              v-on:keyup.13="search()"
+              v-model="criteria.filterText"
+              autocomplete="off"
+            />
+          </div>
+        </div>
       </div>
 
       <div class="sumbit text-right">
